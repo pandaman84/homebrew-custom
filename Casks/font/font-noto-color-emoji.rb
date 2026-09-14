caa1701bb34c7de85ff4e853afeb1e0478352bc5
pandaman84 <69359467+pandaman84@users.noreply.github.com>
2026-09-13 20:24:54 -0700

    Update font-noto-color-emoji.rb

--- Casks/font/font-noto-color-emoji.rb
+++ Casks/font/font-noto-color-emoji.rb
@@ -1,5 +1,5 @@
 cask "font-noto-color-emoji" do
-  version "1.0.0"
+  version "1.0.1"
   sha256 :no_check
 
   url "https://github.com/google/fonts/raw/main/ofl/notocoloremoji/NotoColorEmoji-Regular.ttf"
