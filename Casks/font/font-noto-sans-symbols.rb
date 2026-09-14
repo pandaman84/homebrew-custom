82c73951386e4db9742bbff34b231cee808157d1
pandaman84 <69359467+pandaman84@users.noreply.github.com>
2026-09-13 20:26:02 -0700

    Bump Noto Sans Symbols font version to 1.0.1

--- Casks/font/font-noto-sans-symbols.rb
+++ Casks/font/font-noto-sans-symbols.rb
@@ -1,5 +1,5 @@
 cask "font-noto-sans-symbols" do
-  version "1.0.0"
+  version "1.0.1"
   sha256 :no_check
 
   url "https://github.com/google/fonts/raw/main/ofl/notosanssymbols/NotoSansSymbols%5Bwght%5D.ttf"
