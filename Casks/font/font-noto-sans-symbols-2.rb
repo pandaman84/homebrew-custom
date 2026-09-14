d8d44d147f156a8cc6ca62872c07ff4d2061f142
pandaman84 <69359467+pandaman84@users.noreply.github.com>
2026-09-13 20:25:46 -0700

    Update font-noto-sans-symbols-2.rb

--- Casks/font/font-noto-sans-symbols-2.rb
+++ Casks/font/font-noto-sans-symbols-2.rb
@@ -1,5 +1,5 @@
 cask "font-noto-sans-symbols-2" do
-  version "1.0.0"
+  version "1.0.1"
   sha256 :no_check
 
   url "https://github.com/google/fonts/raw/main/ofl/notosanssymbols2/NotoSansSymbols2-Regular.ttf"
